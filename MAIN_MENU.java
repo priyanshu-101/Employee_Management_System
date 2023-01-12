a3f140ebb2f24fe40cb58b16f78c86844d0513a0
Priyanshu Agarwal <109196864+priyanshu-101@users.noreply.github.com>
2023-01-12 18:21:20 +0530

    Create MAIN_MENU.java

---
+++ MAIN_MENU.java
@@ -0,0 +1,15 @@
+class MainMenu{
+    public void menu(){
+        System.out.println("\t********************************");
+        System.out.println();
+        System.out.println("\t\tEMPLOYEE MANAGEMENT SYSTEM");
+        System.out.println();
+        System.out.println("------------------------------");
+        System.out.println("\t\tBY PRIYANSHU AGARWAL");
+        System.out.println("------------------------------");
+        System.out.println("Press 1 : To Add an Employee Details");
+        System.out.println("Press 2 : To See an Employee Details ");
+        System.out.println("Press 3 : To Remove an Employee");
+        System.out.println("Press 4 : To Exit the EMS Portal");
+    }
+}
